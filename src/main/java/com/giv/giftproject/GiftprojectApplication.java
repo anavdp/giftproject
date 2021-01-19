0b69a51cbcf29ad845dbedb760fddacbfaef1260
Ana Victoria De Palma <anavdepalma@gmail.com>
2021-01-19 23:51:25 +0100

    First commit

---
+++ src/main/java/com/giv/giftproject/GiftprojectApplication.java
@@ -0,0 +1,13 @@
+package com.giv.giftproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftprojectApplication.class, args);
+	}
+
+}
