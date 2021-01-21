ee9929b1af8244386efe58bc7cd9ea0a8ec2bb23
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-01-22 00:10:33 +0100

    creating user entity

---
+++ sql/insertUser.sql
@@ -0,0 +1,18 @@
+INSERT INTO `gift_project`.`USER`(
+NAME,
+LASTNAME,
+BIRTHDATE,
+EMAIL,
+PHONENUMBER,
+GENDER,
+PRONOUN) 
+VALUES (
+'Ana Victoria',
+'De Palma',
+'1994-08-30',
+'anavdepalma@gmail.com',
+'625847745',
+'FEMALE',
+'SHE');
+
+COMMIT;
