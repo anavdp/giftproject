2ce08c6d37acae95ed665d3b872ab18e859fed37
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-01-27 00:34:24 +0100

    minor changes, phone_number column

--- src/main/java/com/giv/giftproject/domain/model/User.java
+++ src/main/java/com/giv/giftproject/domain/model/User.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
-
 import com.giv.giftproject.domain.enums.Gender;
 import com.giv.giftproject.domain.enums.Pronoun;
 
