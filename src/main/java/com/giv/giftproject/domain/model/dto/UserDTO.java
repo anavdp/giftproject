e401c08e40c281fb4c14b08fbd84a99b2b928cb5
Ana Victoria <anavdepalma@gmail.com>
2021-02-02 00:36:48 +0100

    adding some validation to birthdate

--- src/main/java/com/giv/giftproject/domain/model/dto/UserDTO.java
+++ src/main/java/com/giv/giftproject/domain/model/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.giv.giftproject.domain.model.dto;
 import java.time.LocalDate;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
 import com.giv.giftproject.domain.enums.Gender;
 import com.giv.giftproject.domain.enums.Pronoun;
 
@@ -16,6 +19,8 @@ public class UserDTO {
     @Email(message = "Please provide a valid email")
     private String email;
 
+    @Past
+    @NotNull
     private LocalDate birthdate;
 
     private String phoneNumber;
