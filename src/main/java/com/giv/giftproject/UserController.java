87cf50057c9d4cb1f3be9a4cb0d050fd1fa3f332
Ignacio Arjona <narjonacruz@gmail.com>
2021-02-07 01:23:38 +0100

    refactor

--- src/main/java/com/giv/giftproject/UserController.java
+++ src/main/java/com/giv/giftproject/UserController.java
@@ -5,16 +5,16 @@ import com.giv.giftproject.domain.model.dto.UserDTO;
 import com.giv.giftproject.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import javax.validation.Valid;
 
 @Controller
 public class UserController {
